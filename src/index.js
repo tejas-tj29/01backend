f831f0a60a7a6512ebb9e5d31a2738f19e99f5cc
tejas-tj29 <btech10225.25@bitmesra.ac.in>
2026-01-12 21:52:10 +0530

    app connected to server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import dbConnect from './db/db_connect.js'
 import dotenv from 'dotenv'
-
+import {app} from './app.js'
 dotenv.config({
     path:'./env'
 })
-dbConnect();
+dbConnect()
+.then(()=>{
+    app.on("error",(error) => {
+        console.log("Error:",error);
+        throw error
+    })
+    app.listen((process.env.port||5000),()=>{
+        console.log(`Server is running at Port:${process.env.port}`);
+    })
+    app.get('/',(req,res)=>{
+        res.send("DB connected and server is running");
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB failed to connect",error);
+})
