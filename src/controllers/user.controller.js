f227011bcbcaea372ab36390c63724af85afac68
tejas-tj29 <btech10225.25@bitmesra.ac.in>
2026-04-02 19:35:47 +0530

    removed bugs seeing postman errors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser = asyncHandler(async (req,res) => {
         throw new ApiError(409, "Username or email already exists");
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
     
     let coverImageLocalPath;
     if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
